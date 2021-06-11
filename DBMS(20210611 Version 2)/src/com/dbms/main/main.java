c943dd2e81f181607fd44704c34fd00e1099e6ac
WANGKANG1717 <85092561+WANGKANG1717@users.noreply.github.com>
2021-06-11 15:24:42 +0800

    Add files via upload

---
+++ DBMS(20210611 Version 2)/src/com/dbms/main/main.java
@@ -0,0 +1,10 @@
+package com.dbms.main;
+
+import com.dbms.view.DBMSWindow;
+import com.dbms.view.Login;
+
+public class main {
+    public static void main(String[] args) {
+        new Login();
+    }
+}
